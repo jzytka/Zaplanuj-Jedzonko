f047956b850b7e577992932ef106d5a69806f9be
Marcin Osuchowski <demon2444@gmail.com>
2019-08-27 15:09:44 +0200

    Poprawione linki do stron jsp

---
+++ src/main/java/pl/coderslab/model/AdminTest.java
@@ -0,0 +1,17 @@
+package pl.coderslab.model;
+
+import pl.coderslab.dao.AdminDao;
+//import pl.coderslab.dao.PlanDao;
+
+import java.util.Collections;
+import java.util.List;
+
+public class AdminTest {
+    public static void main(String[] args) {
+       //int wynik =  PlanDao.countPlans(1);
+       // System.out.println(wynik);
+        Admin admin = new Admin();
+        admin = AdminDao.read(1);
+        System.out.println(admin.toString());
+    }
+}
